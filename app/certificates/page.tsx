318c38d9f08d78ff77fb8ac620308b6d0448deeb
Valentin Dirzu <dirzu.valentin@gmail.com>
2025-05-15 21:15:47 +0100

    fix: cert images

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -6,6 +6,7 @@ import tpImage3 from "@/public/images/certificates/tp-3.jpg";
 import tpImage4 from "@/public/images/certificates/tp-4.jpg";
 import tpImage5 from "@/public/images/certificates/tp-5.jpg";
 import tpImage6 from "@/public/images/certificates/tp-6.jpg";
+import tpImage7 from "@/public/images/certificates/tp-7.jpg";
 
 import { useTranslation } from "react-i18next";
 import CardSlider from "../components/card-slider";
@@ -39,6 +40,10 @@ export default function CertificatesPage() {
     },
     {
       image: tpImage6.src,
+      alt: t("certificates.testingProcess.slides.durabilityTesting.alt"),
+    },
+    {
+      image: tpImage7.src,
       alt: t("certificates.testingProcess.slides.durabilityTesting.alt"),
     },
   ];
